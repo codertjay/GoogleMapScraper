2d3440458f9ea8ebe9d9b8f52335b4c11f173a7a
Codertjay <codertjay@gmail.com>
2023-04-07 10:45:02 +0100

    added some functionlirty

--- map_scrapers/admin.py
+++ map_scrapers/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from map_scrapers.models import History
 
-admin.site.register(History)
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'business_name', 'latitude', 'longitude', 'timestamp')
+    list_filter = ('user', 'business_name', 'municipality')
+    search_fields = ('business_name', 'municipality')
+
+
+admin.site.register(History,HistoryAdmin)
