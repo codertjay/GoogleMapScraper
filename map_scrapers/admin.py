234524847143c8037374242ca546f945844fb9e8
Codertjay <codertjay@gmail.com>
2023-04-21 11:21:25 +0100

    adde changes

--- map_scrapers/admin.py
+++ map_scrapers/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from map_scrapers.models import History
+from map_scrapers.models import History,SearchInfo
 
 
 class HistoryAdmin(admin.ModelAdmin):
@@ -11,3 +11,4 @@ class HistoryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(History,HistoryAdmin)
+admin.site.register(SearchInfo)
