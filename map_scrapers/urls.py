3e5e55dc3418841f4078e75e5d3595c50c0ba7ce
Codertjay <codertjay@gmail.com>
2023-04-23 13:28:01 +0100

    adde changes

--- map_scrapers/urls.py
+++ map_scrapers/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import DownloadUserCSVView, DownloadAllCSVView, get_csv_sample, ExportSearchInfoCSV
 
 urlpatterns = [
-    path('', views.map_view, name='map'),
+    path('map/', views.map_view, name='map'),
     path('search/', views.search_api, name='search_api'),
     path('place_detail/', views.place_detail, name='place_detail'),
     path('autocomplete/', views.autocomplete_api, name='autocomplete'),
@@ -12,7 +12,7 @@ urlpatterns = [
     path('<int:id>/history/', views.HistoryListView.as_view(), name='history_list'),
     path('admin_history/', views.AdminHistoryListView.as_view(), name='admin_history'),
     path('history_update/', views.HistoryUpdateView.as_view(), name='history_update'),
-    path('search_dashboard/', views.SearchDashboardView.as_view(), name='search_dashboard'),
+    path('', views.SearchDashboardView.as_view(), name='search_dashboard'),
     path('history_delete/', views.HistoryDeleteView.as_view(), name='history_delete'),
     path('<int:id>/search_info_download/', ExportSearchInfoCSV.as_view(), name='search_info_download'),
     path('history_download/', DownloadUserCSVView.as_view(), name='csv_download'),
